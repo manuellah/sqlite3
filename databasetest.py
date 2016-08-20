017a8dcc12df47bf934c5f76a69c13773a6a9264
manuellah <emmanumuthui@gmail.com>
2016-08-20 17:38:45 +0300

    added some try except to cater for database sefety

--- databasetest.py
+++ databasetest.py
@@ -1,22 +1,22 @@
 import sqlite3
 
 def Main():
-    con = sqlite3.connect("test.db") #create connection
-    cur = con.cursor() #creates a new cursor object which executes sql stmt for us
-    cur.execute("SELECT SQLITE_VERSION()")
-    
-    data = cur.fetchone()
-    
-    print ("SQLITE version  "+str(data))
-    
-    con.close()
+    try:
+        con = sqlite3.connect("test.db") #create connection
+        cur = con.cursor() #creates a new cursor object which executes sql stmt for us
+        cur.execute("SELECT SQLITE_VERSION()")
+
+        data = cur.fetchone()
+
+        print ("SQLITE version  "+str(data))
+    except:
+        if con:
+            con.rollback()
+    finally:        
+        if con:    
+            con.close()
     
     
 if __name__ == "__main__":
     Main()
-    
-    
-    
-     
-    
-    
+   
