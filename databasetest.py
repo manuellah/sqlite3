9fa7daf681f77f1b14b6e101e6104012628f6c23
manuellah <emmanumuthui@gmail.com>
2016-08-20 17:57:21 +0300

    created a table and added(inserted some data in it)

--- databasetest.py
+++ databasetest.py
@@ -4,14 +4,24 @@ def Main():
     try:
         con = sqlite3.connect("test.db") #create connection
         cur = con.cursor() #creates a new cursor object which executes sql stmt for us
-        cur.execute("SELECT SQLITE_VERSION()")
+        cur.execute("CREATE TABLE pets(Id INT, Name TEXT, Price INT)")
+        cur.execute("INSERT INTO pets VALUES(1, 'cat',400)")
+        cur.execute("INSERT INTO pets VALUES(2, 'dog',600)")
+        cur.execute("INSERT INTO pets VALUES(3,'Dolly',450)")
+        
+        con.commit()
+        
+        cur.execute("SELECT * FROM pets")
 
-        data = cur.fetchone()
-
-        print ("SQLITE version  "+str(data))
+        data = cur.fetchall()
+        
+        for row in data:
+             print(row)
+        
     except:
         if con:
             con.rollback()
+            print("There was a problem with the SQL db")
     finally:        
         if con:    
             con.close()
